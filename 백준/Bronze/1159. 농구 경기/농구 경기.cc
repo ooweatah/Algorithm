15bbf55b7b4a9b69429006e82392f40cea100f40
TAEWOO HA <122782748+ooweatah@users.noreply.github.com>
2023-01-18 19:55:09 +0900

    [Bronze II] Title: 농구 경기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1159. 농구 경기/농구 경기.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int n,cnt[26];
+string s, ret;
+
+
+int main(){
+	cin>>n;
+	for(int i=0;i<n;i++){
+		cin>>s;
+		cnt[s[0]-'a']++;
+	}
+	for(int i=0;i<26;i++)if(cnt[i]>=5) ret+= (i+'a');
+	if(ret.size()){
+		cout<<ret<<" ";
+	}
+	else cout<<"PREDAJA"<<" ";
+	return 0;
+}
