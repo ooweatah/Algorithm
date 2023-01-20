e890eed9602d09134ea274cf846d3489336953c2
TAEWOO HA <122782748+ooweatah@users.noreply.github.com>
2023-01-20 21:09:07 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -2,26 +2,31 @@
 using namespace std; 
 string s, ret; 
 int alpha[100], holnum; 
-char mid;
+string hol,zzac;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cin >> s;
 	for(char a : s)alpha[a]++;
-	for(char i = 'Z'; i >= 'A'; i--){
+	
+	for(char i = 'A'; i <= 'Z'; i++){
 		if(alpha[i]){
 			if(alpha[i] % 2==1){
-				mid = char(i);holnum++;
-				alpha[i]--;
+				holnum++;
+				hol+=i;
 			}
 			if(holnum == 2)break;
-			for(int j = 0; j < alpha[i]; j += 2){
-				ret = char(i) + ret; 
-				ret += char(i);
+			for(int j = 0; j < alpha[i]/2;j++){
+				zzac+=i;
 			}
 		}
 	}
-	if(mid)ret.insert(ret.begin()+ ret.size() / 2, mid);
 	if(holnum == 2)cout << "I'm Sorry Hansoo\n";
-	else cout << ret << "\n"; 
+	else
+	{
+	cout<<zzac;
+	cout<<hol;
+	reverse(zzac.begin(),zzac.end());
+	cout<<zzac;
+}
 }
