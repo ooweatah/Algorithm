f4299970ad74aa87f00d4b0e658282069a52ebf3
TAEWOO HA <122782748+ooweatah@users.noreply.github.com>
2023-02-09 18:41:46 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int n) {
+    string answer = "";
+    for(int i = 0;i<=my_string.size()-1;i++){
+        for(int j = 0;j<n;j++ )
+        answer.push_back(my_string[i]) ;
+    }
+    return answer;
+}
