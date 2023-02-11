9a4d3d9af6310895d909673b114e35b6026f8e4d
TAEWOO HA <122782748+ooweatah@users.noreply.github.com>
2023-02-12 01:24:14 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    for(int i = 0; i < my_string.size(); i++)
+    {
+        if(answer.find(my_string[i]) == string::npos)
+            answer.push_back(my_string[i]);
+    }
+    return answer;
+}
