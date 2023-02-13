0c4beaf8d4f3a1d3b020db5490c1335904714b5f
TAEWOO HA <122782748+ooweatah@users.noreply.github.com>
2023-02-13 20:54:35 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.02 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.cpp
@@ -0,0 +1,30 @@
+
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+int solution(string my_string) {
+    int answer = 0;
+
+    stringstream ss;
+    ss.str(my_string);
+
+    ss>>answer;
+
+    char ch;
+    int tmp = 0;
+    while(ss)
+    {
+        if(ch == '+')
+        {
+            answer += tmp;
+        }
+        else if(ch == '-')
+        {
+            answer -= tmp;
+        }
+        ss >> ch >> tmp;
+    }
+    return answer;
+}
