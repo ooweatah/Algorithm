aaaa3f43b884355c68360491ef22cf538a80b3e2
TAEWOO HA <122782748+ooweatah@users.noreply.github.com>
2023-02-14 15:10:13 +0900

    [level 0] Title: 로그인 성공?, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.cpp
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.cpp
@@ -1,17 +1,24 @@
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
 
 string solution(vector<string> id_pw, vector<vector<string>> db) {
+    map<string,string> member;
+    string answer = "";
     for(auto info : db){
-        if(info[0] ==id_pw[0]&&info[1]==id_pw[1]){
-            return "login";
-        }
-        else if(info[0]==id_pw[0]&&info[1]!=id_pw[1]){
-            return "wrong pw";
-        }
-
+        member[info[0]]=info[1];
     }
-    return "fail";
+if (member.find(id_pw[0]) == member.end()) {
+    // 아이디가 없는 경우
+    answer = "fail";
+} else if (member[id_pw[0]] == id_pw[1]) {
+    // 아이디와 비밀번호가 일치하는 경우
+    answer = "login";
+} else {
+    // 아이디는 일치하지만 비밀번호가 일치하지 않는 경우
+    answer = "wrong pw";
+}
+    return answer;
 }
