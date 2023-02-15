eb1c2dee2a544d64f3bef7fa0154f64291c116f5
TAEWOO HA <122782748+ooweatah@users.noreply.github.com>
2023-02-15 16:27:13 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.c
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.c
@@ -5,11 +5,5 @@
 // arr_len은 배열 arr의 길이입니다.
 double solution(int arr[], size_t arr_len) {
     double answer = 0;
-    double sum = 0;
-    for(int i =0;i<arr_len;i++){
-        sum += arr[i];
-    }
-    answer = sum/arr_len;
-    
     return answer;
 }
