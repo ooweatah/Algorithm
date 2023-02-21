15011ec41e90982d289382d2a3bce6ff68ed2d40
TAEWOO HA <122782748+ooweatah@users.noreply.github.com>
2023-02-21 22:03:17 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.cpp
@@ -0,0 +1,16 @@
+#include <string>
+using namespace std;
+
+string solution(int a, int b) {
+
+    int month[] = {31,29,31,30,31,30,31,31,30,31,30,31};
+    string day[] = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+    
+    int sum = b - 1;
+    
+    for(int i=0; i<a-1; i++){
+        sum += month[i];
+    }
+
+    return day[sum%7];
+}
--- 프로그래머스/lv1/12901. 2016년/README.md
+++ 프로그래머스/lv1/12901. 2016년/README.md
@@ -1,10 +1,10 @@
 # [level 1] 2016년 - 12901 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.01 ms
+메모리: 4.21 MB, 시간: 0.01 ms
 
 ### 구분
 
