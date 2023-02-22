41b9ee0742ce24d0bd84cf1668e5c1009846422a
TAEWOO HA <122782748+ooweatah@users.noreply.github.com>
2023-02-23 01:08:00 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
@@ -17,3 +17,24 @@ vector<int> solution(int k, vector<int> score) {
     return answer;
 
 }
+/*#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<int> solution(int k, vector<int> score) {
+    vector<int> answer;
+    priority_queue<int, vector<int>, greater<int>> rank;
+    for(int i = 0; i < score.size(); i++){
+        if(i < k) rank.push(score[i]);
+        else{
+            if(rank.top() <= score[i]){
+                rank.pop();
+                rank.push(score[i]);
+            }
+        }
+        answer.push_back(rank.top());
+    }
+    return answer;
+}*/
