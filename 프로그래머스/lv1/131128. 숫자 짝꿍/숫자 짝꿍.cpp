f5e5cb2726e785839c6c6ef7bb9f33097606c5ab
TAEWOO HA <122782748+ooweatah@users.noreply.github.com>
2023-02-24 01:56:25 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.cpp
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.cpp
@@ -1,29 +1,33 @@
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <iostream>
 
 using namespace std;
 
-int x[10];
-int y[10];
-
-int* countNum(int arr[], string s){
-    for(int i=0; i<s.size(); i++) arr[s[i]-'0']++;
-    return arr;
-}
-
 string solution(string X, string Y) {
     string answer = "";
-    
-    countNum(x, X);
-    countNum(y, Y);
-    
-    for(int i=9; i>=0; i--){
-        int num=min(x[i], y[i]);
-        for(int j=0; j<num; j++)
-            answer+=to_string(i);
+    sort(X.begin(), X.end());
+    sort(Y.begin(), Y.end());
+    while((X.empty() == 0) && (Y.empty() == 0)){
+        if(X.back() == Y.back()){
+            answer.push_back(X.back());
+            X.pop_back();
+            Y.pop_back();
+        } else if (X.back() > Y.back()){
+            X.pop_back();
+        } else {
+            Y.pop_back();
+        }
     }
-    
-    if(answer=="") return "-1";
-    else if(answer[0]=='0') return "0";
-    else return answer;
+    if (answer.empty()) {
+        return "-1";
+    }
+    while(answer[0] == '0'){
+        answer.erase(0, 1);
+    }
+    if (answer.empty()) {
+        return "0";
+    } 
+    return answer;
 }
