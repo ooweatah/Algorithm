1ed395abe479b81aa84a6312fc971438326ed088
TAEWOO HA <122782748+ooweatah@users.noreply.github.com>
2023-02-25 23:42:14 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.08 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/README.md
+++ 프로그래머스/unrated/136798. 기사단원의 무기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 기사단원의 무기 - 136798 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798?language=cpp) 
 
 ### 성능 요약
 
-메모리: 3.68 MB, 시간: 0.06 ms
+메모리: 4.14 MB, 시간: 0.08 ms
 
 ### 구분
 
--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.cpp
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.cpp
@@ -10,11 +10,10 @@ int solution(int number, int limit, int power) {
         int str = 0;
         for(int j=1;j*j<=i;j++){
             if(i%j==0){
-                int share = i/j;
-                if((share*share)==i){
+                str++;
+                if(j*j<i){
                     str++;
                 }
-                else str+=2;
                 
             }
         }
