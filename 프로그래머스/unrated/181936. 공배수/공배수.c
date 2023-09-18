e65b1cc1402de66167938742250fcfb83dd35117
TAEWOO HA <122782748+ooweatah@users.noreply.github.com>
2023-09-19 04:09:51 +0900

    [unrated] Title: 공배수, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181936. 공배수/공배수.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int number, int n, int m) {
+    int answer = 0;
+    if(number % n ==0 && number % m ==0){
+        return 1;
+    }
+    else 0;
+    return answer;
+}
