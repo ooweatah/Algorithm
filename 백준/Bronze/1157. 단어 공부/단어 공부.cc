c1a7c7e34c33fcf6d2f42eb18030e1d439c84b5d
TAEWOO HA <122782748+ooweatah@users.noreply.github.com>
2023-10-10 03:38:58 +0900

    [Bronze I] Title: 단어 공부, Time: 16 ms, Memory: 1972 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.cc
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+int main()
+{
+    char N[1000001];
+    int a[26] = {0};
+    scanf("%s",N);
+    
+    //for(int i = 0; i<strlen(N);i++){
+     //   if(N[i]>='a' && N[i]<='z'){
+      //      N[i] = N[i] - ('A'-'a');
+      //  }
+   // }
+    for (int i = 0; N[i]; i++) {
+        char c = tolower(N[i]); // 대문자를 소문자로 변환
+        if (isalpha(c)) {
+            a[c - 'a']++;
+        }
+    }
+    int maxCount = 0;
+    char maxA = '?';
+    
+    for(int i = 0 ; i < 26 ; i++){
+        if(a[i] > maxCount){
+            maxCount = a[i];
+            maxA = 'A' + i ; 
+        }
+        else if(a[i] == maxCount){
+            maxA = '?';
+        }
+    }
+    printf("%c",maxA);
+    return 0;
+}
